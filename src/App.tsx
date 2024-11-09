ec70f79d868f969578115bf6728d0cd5f7b11d45
Nour Ahmed <nour.elhabbal9@gmail.com>
2024-11-09 21:04:03 +0200

    Feat: Clock implementation

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useWakeLock from "react-use-wake-lock";
+
+function App() {
+  const date = new Date();
+  const { request, isLocked } = useWakeLock();
+
+  const [time, setTime] = useState(
+    `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
+  );
+
+  useEffect(() => {
+    request();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(
+        `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds() + 1}`
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [time]);
+
+  return (
+    <Flex w="100vw" h="100vh" justify="center" align="center">
+      <Box
+        as="h1"
+        fontSize="8xl"
+        fontFamily="'Orbitron', serif"
+        color="red.600"
+        fontWeight="800"
+      >
+        {time}
+      </Box>
+    </Flex>
+  );
+}
+
+export default App;
