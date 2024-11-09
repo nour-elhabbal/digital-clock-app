bc48b9bc06322912d53abd8798a2c31b7597e414
Nour Ahmed <nour.elhabbal9@gmail.com>
2024-11-09 21:24:30 +0200

    Feat: Added fullscreen feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import useWakeLock from "react-use-wake-lock";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 function App() {
   const date = new Date();
   const { request } = useWakeLock();
+  const fullScreen = useFullScreenHandle();
 
   const [time, setTime] = useState(
     `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
@@ -25,17 +27,22 @@ function App() {
   }, [time]);
 
   return (
-    <Flex w="100vw" h="100vh" justify="center" align="center">
-      <Box
-        as="h1"
-        fontSize="8xl"
-        fontFamily="'Orbitron', serif"
-        color="red.600"
-        fontWeight="800"
-      >
-        {time}
-      </Box>
-    </Flex>
+    <FullScreen handle={fullScreen}>
+      <Flex w="100vw" h="100vh" justify="center" align="center">
+        <Box
+          as="h1"
+          fontSize="8xl"
+          fontFamily="'Orbitron', serif"
+          color="red.600"
+          fontWeight="800"
+          onClick={() => {
+            fullScreen.enter();
+          }}
+        >
+          {time}
+        </Box>
+      </Flex>
+    </FullScreen>
   );
 }
 
